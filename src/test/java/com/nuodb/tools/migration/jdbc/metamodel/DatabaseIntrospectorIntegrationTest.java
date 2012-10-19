f854c5f4e10bcab023e1a546e8686d0f70e3c070
Sergey Bushik <tazija@gmail.com>
2012-10-19 15:38:04 +0300

    Integration test for dump added

---
+++ src/test/java/com/nuodb/tools/migration/jdbc/metamodel/DatabaseIntrospectorIntegrationTest.java
@@ -0,0 +1,88 @@
+package com.nuodb.tools.migration.jdbc.metamodel;
+
+
+import com.nuodb.tools.migration.jdbc.connection.DriverManagerConnectionProvider;
+import com.nuodb.tools.migration.spec.DriverManagerConnectionSpec;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.Map;
+
+public class DatabaseIntrospectorIntegrationTest {
+
+
+    private Connection connection;
+    private DatabaseIntrospector introspector;
+
+    @Before
+    public void setUp() throws Exception {
+        DriverManagerConnectionSpec mysql = new DriverManagerConnectionSpec();
+        mysql.setDriver("com.mysql.jdbc.Driver");
+        mysql.setUrl("jdbc:mysql://localhost:3306/test");
+        mysql.setUsername("root");
+
+        DriverManagerConnectionSpec nuodb = new DriverManagerConnectionSpec();
+        nuodb.setDriver("com.nuodb.jdbc.Driver");
+        nuodb.setUrl("jdbc:com.nuodb://localhost/test");
+        nuodb.setUsername("dba");
+        nuodb.setPassword("goalie");
+
+
+        DriverManagerConnectionProvider connectionProvider = new DriverManagerConnectionProvider(nuodb);
+
+        connection = connectionProvider.getConnection();
+        Assert.assertNotNull(connection);
+        Assert.assertNotNull(connection.getMetaData());
+        Assert.assertFalse(connection.isClosed());
+
+        introspector = new DatabaseIntrospector();
+        introspector.withConnection(connection);
+    }
+
+    @Test
+    public void testReadInfo() throws Exception {
+
+        final Database database = new Database();
+        introspector.readInfo(connection.getMetaData(), database);
+
+        final DriverInfo driverInfo = database.getDriverInfo();
+        final DatabaseInfo databaseInfo = database.getDatabaseInfo();
+        Assert.assertNotNull(driverInfo);
+        Assert.assertNotNull(databaseInfo);
+        Assert.assertNotNull(driverInfo.getName());
+        Assert.assertNotNull(driverInfo.getMajorVersion());
+        Assert.assertNotNull(driverInfo.getMinorVersion());
+        Assert.assertNotNull(driverInfo.getVersion());
+        Assert.assertNotNull(databaseInfo.getProductName());
+        Assert.assertNotNull(databaseInfo.getMajorVersion());
+        Assert.assertNotNull(databaseInfo.getMinorVersion());
+    }
+
+    @Test
+    public void testReadObjects() throws Exception {
+        final Database database = new Database();
+        introspector.readObjects(connection.getMetaData(), database);
+
+
+        final Map<Name,Catalog> catalogs = database.getCatalogs();
+        Assert.assertFalse(catalogs.isEmpty());
+
+        final Collection<Schema> schemas = database.listSchemas();
+        Assert.assertFalse(schemas.isEmpty());
+
+        final Collection<Table> tables = database.listTables();
+        Assert.assertFalse(tables.isEmpty());
+
+    }
+
+
+
+    @After
+    public void tearDown() throws Exception {
+        connection.close();
+    }
+}
