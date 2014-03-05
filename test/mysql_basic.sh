f89e96304f111d9fd27f90e80d8a47442a907694
Tommy Reilly <treilly@nuodb.com>
2014-03-05 12:38:30 -0500

    Refactor travis setup so that we can easily test it outside travis.

---
+++ test/mysql_basic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+. ./test/.travis_env
+mysql -u$SOURCE_USERNAME -e 'drop database nuodbtest;'
+mysql -u$SOURCE_USERNAME -e 'create database nuodbtest;'
+mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/nuodbtest.sql
+mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/precision.sql
+${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator dump --time.zone=EST --source.driver=${SOURCE_DRIVER} --source.url=${SOURCE_URL}  --source.catalog=nuodbtest --source.username=${SOURCE_USERNAME} --output.type=bson --output.path=/var/tmp/dump.cat
+${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator load --time.zone=EST --target.url=${NUODB_URL} --target.username=${NUODB_USERNAME} --target.password=${NUODB_PASSWORD} --input.path=/var/tmp/dump.cat
+mvn -Pmysql-integration-tests test
---
+++ test/nuodb_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+. ./test/.travis_env
+wget -q http://download.nuohub.org/nuodb-2.0.3.linux.x64.deb --output-document=/var/tmp/nuodb.deb
+sudo dpkg -i /var/tmp/nuodb.deb
+${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process sm host localhost database test archive /var/tmp/nuodb initialize true"
+${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process te host localhost database test options '--dba-user ${NUODB_USERNAME} --dba-password ${NUODB_PASSWORD}'"
