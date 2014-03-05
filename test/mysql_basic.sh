588642d61a407222906f788ff53a693cc7c2b5f0
Tommy Reilly <treilly@nuodb.com>
2014-03-05 14:44:07 -0500

    Clean up properly before running tests

--- test/mysql_basic.sh
+++ test/mysql_basic.sh
@@ -1,9 +1,10 @@
-#!/bin/sh -e
+#!/bin/bash -e
 . ./test/.travis_env
-mysql -u$SOURCE_USERNAME -e 'drop database nuodbtest;'
+mysql -u$SOURCE_USERNAME -e 'drop database if exists nuodbtest;'
 mysql -u$SOURCE_USERNAME -e 'create database nuodbtest;'
 mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/nuodbtest.sql
 mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/precision.sql
+nuodrop ${NUODB_SCHEMA}
 ${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator dump --time.zone=EST --source.driver=${SOURCE_DRIVER} --source.url=${SOURCE_URL}  --source.catalog=nuodbtest --source.username=${SOURCE_USERNAME} --output.type=bson --output.path=/var/tmp/dump.cat
 ${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator load --time.zone=EST --target.url=${NUODB_URL} --target.username=${NUODB_USERNAME} --target.password=${NUODB_PASSWORD} --input.path=/var/tmp/dump.cat
 mvn -Pmysql-integration-tests test
