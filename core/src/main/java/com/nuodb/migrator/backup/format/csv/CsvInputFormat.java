1aee2fc54ade0409502683f520424bb139e8cae2
tazija <tazija@gmail.com>
2014-06-12 12:57:53 +0300

    Apache CSV is taken from local repo

--- core/src/main/java/com/nuodb/migrator/backup/format/csv/CsvInputFormat.java
+++ core/src/main/java/com/nuodb/migrator/backup/format/csv/CsvInputFormat.java
@@ -36,7 +36,11 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 import java.util.List;
 
