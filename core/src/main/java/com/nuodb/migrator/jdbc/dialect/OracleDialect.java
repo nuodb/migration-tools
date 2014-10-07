04f49e9f49b584a55741670ff87a3889edb3e17e
tazija <tazija@gmail.com>
2014-10-07 18:02:48 +0300

    Removed unnecessary main() method

--- core/src/main/java/com/nuodb/migrator/jdbc/dialect/OracleDialect.java
+++ core/src/main/java/com/nuodb/migrator/jdbc/dialect/OracleDialect.java
@@ -222,22 +222,4 @@ public class OracleDialect extends SimpleDialect {
     public RowCountHandler createRowCountHandler(Table table, Column column, String filter, RowCountType rowCountType) {
         return new OracleTableRowCountHandler(this, table, column, filter, rowCountType);
     }
-
-    public static void main(String[] args) {
-        OracleDialect oracleDialect = new OracleDialect(new DatabaseInfo());
-        JdbcTypeNameMap jdbcTypeNameMap = oracleDialect.getJdbcTypeNameMap();
-        Collection<JdbcTypeName> jdbcTypeNames = jdbcTypeNameMap.getJdbcTypeNames();
-        for (Iterator<JdbcTypeName> iterator = jdbcTypeNames.iterator(); iterator.hasNext(); ) {
-            JdbcTypeName type = iterator.next();
-            if (type instanceof JdbcTypeNameTemplate) {
-                JdbcTypeNameTemplate template = (JdbcTypeNameTemplate) type;
-                String name = template.getTemplate();
-                JdbcType jdbcType = template.getJdbcType();
-                System.out.println(String.format("%s %d", name, jdbcType.getJdbcTypeDesc().getTypeCode()));
-            } else {
-                System.out.println(" > " + type);
-            }
-
-        }
-    }
 }
