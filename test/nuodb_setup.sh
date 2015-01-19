7c02f329a5c57751edf2f5fcb93adb459dd86106
tazija <tazija@gmail.com>
2015-01-19 15:47:58 -0800

    Reverted back version

--- test/nuodb_setup.sh
+++ test/nuodb_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 . ./test/.travis_env
 
-wget -q http://download.nuohub.org/nuodb_2.1.1.10_amd64.deb --output-document=/var/tmp/nuodb.deb
+wget -q http://download.nuohub.org/nuodb-2.0.4.linux.x64.deb --output-document=/var/tmp/nuodb.deb
 sudo dpkg -i /var/tmp/nuodb.deb
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process sm host localhost database test archive /var/tmp/nuodb initialize true"
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process te host localhost database test options '--dba-user ${NUODB_USERNAME} --dba-password ${NUODB_PASSWORD}'"
