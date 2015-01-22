a7c18a6a4e11f962a51badba45b034f2bb0e1c50
Mahesha Godekere <mgodekere@gmail.com>
2015-01-22 15:37:33 +0530

    MIG-105 Adding nuodb default.properties to enable domain password config which is missing in .dep package. Referencing the same in n nuo setup

--- test/nuodb_setup.sh
+++ test/nuodb_setup.sh
@@ -3,5 +3,16 @@
 
 wget -q http://download.nuohub.org/nuodb_2.1.1.10_amd64.deb --output-document=/var/tmp/nuodb.deb
 sudo dpkg -i /var/tmp/nuodb.deb
+
+/opt/nuodb/etc/nuoagent stop
+/opt/nuodb/etc/nuorestsvc stop
+/opt/nuodb/etc/nuowebconsole stop
+
+sudo cp ./test/default.properties /opt/nuodb/etc/.
+
+/opt/nuodb/etc/nuoagent start
+/opt/nuodb/etc/nuorestsvc start
+/opt/nuodb/etc/nuowebconsole start
+
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process sm host localhost database test archive /var/tmp/nuodb initialize true"
 ${NUODB_HOME}/bin/nuodbmgr --broker localhost --password bird --command "start process te host localhost database test options '--dba-user ${NUODB_USERNAME} --dba-password ${NUODB_PASSWORD}'"
