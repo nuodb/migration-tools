eaeff58da7e2ccf945bbed1a4c0d24f08b112627
Lakshman Javvadi <ljavvadi@nuodb.com>
2015-08-10 11:14:05 +0530

    Update runall.sh

--- build/runall.sh
+++ build/runall.sh
@@ -14,12 +14,23 @@ fi
 echo "Setup Target NuoDB Database Docker ";
 . ${BASE_DIR}/docker/nuodb_docker.sh
 
-echo "Setup MySQL Docker"
-. ${BASE_DIR}/docker/mysql_docker.sh
+#echo "Setup MySQL  Docker"
 
+#. ${BASE_DIR}/docker/mysql_docker.sh
 # Geting mysql jar from maven repo
-wget  -O ../core/mysql.jar http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.35/mysql-connector-java-5.1.35.jar
+#wget  -O ../core/mysql.jar http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.35/mysql-connector-java-5.1.35.jar
 
-${BASE_DIR}/run.sh bson --database mysql_5.1.71 --driver ../core/mysql.jar
-${BASE_DIR}/run.sh xml --database mysql_5.1.71 --driver  ../core/mysql.jar
-${BASE_DIR}/run.sh csv --database mysql_5.1.71 --driver  ../core/mysql.jar
+#${BASE_DIR}/run.sh bson --database mysql_5.1.71 --driver ../core/mysql.jar
+#${BASE_DIR}/run.sh xml --database mysql_5.1.71 --driver  ../core/mysql.jar
+#${BASE_DIR}/run.sh csv --database mysql_5.1.71 --driver  ../core/mysql.jar
+
+
+
+echo "Setup PostgreSQL Docker"
+. ${BASE_DIR}/docker/postgresql_docker.sh
+
+wget  -O ../core/postgresql-9.3-1100-jdbc3.jar http://central.maven.org/maven2/org/postgresql/postgresql/9.3-1100-jdbc3/postgresql-9.3-1100-jdbc3.jar
+
+${BASE_DIR}/run.sh bson --database  postgresql-9.1-901 --driver  ../core/postgresql-9.3-1100-jdbc3.jar
+${BASE_DIR}/run.sh xml --database   postgresql-9.1-901 --driver  ../core/postgresql-9.3-1100-jdbc3.jar
+${BASE_DIR}/run.sh csv --database   postgresql-9.1-901 --driver  ../core/postgresql-9.3-1100-jdbc3.jar
