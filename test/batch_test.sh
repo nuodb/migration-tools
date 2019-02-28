efe8ff4576e770a8f1232ae7abc941652cae45b1
David Bain <dbain@nuodb.com>
2019-02-28 10:01:12 -0500

    Add table locks to integration test

--- test/batch_test.sh
+++ test/batch_test.sh
@@ -7,6 +7,11 @@ mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/precision.sql
 mysql -u$SOURCE_USERNAME nuodbtest < core/src/test/resources/mysql/datatypes.sql
 ${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator dump --time.zone=EST --source.driver=${SOURCE_DRIVER} --source.url=${SOURCE_URL}  --source.catalog=nuodbtest --source.username=${SOURCE_USERNAME} --output.type=bson --output.path=/var/tmp/dump.cat
 
+if [ "$TABLE_LOCK" = "true" ]; then
+    echo "Enforcing table locks"
+    enforceLocks()
+fi
+
 load()
 {
     ${NUODB_MIGRATOR_HOME}/bin/nuodb-migrator load --time.zone=EST --target.url=${NUODB_URL} --target.username=${NUODB_USERNAME} --target.password=${NUODB_PASSWORD} --input.path=/var/tmp/dump.cat $@
